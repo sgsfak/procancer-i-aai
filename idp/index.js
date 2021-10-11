8a4e92beb6b86c265d211824f8ee7b85360d3eed
Stelios G. Sfakianakis <sgsfak@gmail.com>
2021-10-11 17:51:51 +0300

    add client_credentials grant

--- idp/index.js
+++ idp/index.js
@@ -24,6 +24,25 @@ function idpRoutes({redisClient, webKeyPub, webKeyPrivate}) {
         return token;
     }
 
+    const db_client_registration = function(client_id) {
+        // Check client_id, and retrieve the client registration info
+        // based on this:
+        let client_registration = null;
+        let error = null;
+        try {
+            let {rows} = await db.query("SELECT * FROM clients WHERE active IS TRUE AND id=$1",
+                                        [client_id]);
+            if (rows && rows.length != 0) {
+                client_registration = rows[0];
+            }
+        }
+        catch (e) {
+            console.log("%O", e);
+            error = e;
+        }
+        return [client_registration, error];
+    }
+
     const router = express.Router();
     router.get("/certs", (req, res) => {
         let jwk = JSONWebKey.fromPEM(webKeyPub).toJSON();
@@ -64,32 +83,24 @@ function idpRoutes({redisClient, webKeyPub, webKeyPrivate}) {
         client_id = client_id || '';
         audience = audience || client_id; // The access token will have the 'aud' claim
 
-
         // Check client_id, and retrieve the client registration info
         // based on this:
-        let client_registration;
-        try {
-            let {rows} = await db.query("SELECT * FROM clients WHERE active IS TRUE AND id=$1",
-                                        [client_id]);
-
-            if (!rows || rows.length == 0) {
-
-                /* See https://tools.ietf.org/html/rfc6749#section-4.1.2.1 :
-                If the request fails due to a missing, invalid, or mismatching
-                redirection URI, or if the client identifier is missing or invalid,
-                the authorization server SHOULD inform the resource owner of the
-                error and MUST NOT automatically redirect the user-agent to the
-                invalid redirection URI.
-                */
-                res.status(400).render('idp_error', { user: null, error: `No active client registration for id: '${client_id}'` });
-                return;
-            }
-            client_registration = rows[0];
-        }
-        catch (e) {
+        let [client_registration, error] = db_client_registration(client_id);
+        if (error) {
             console.log("%O", e);
             res.status(500).send("Database error!!");
             return;
+        }
+        if (!client_registration) {
+            /* See https://tools.ietf.org/html/rfc6749#section-4.1.2.1 :
+            If the request fails due to a missing, invalid, or mismatching
+            redirection URI, or if the client identifier is missing or invalid,
+            the authorization server SHOULD inform the resource owner of the
+            error and MUST NOT automatically redirect the user-agent to the
+            invalid redirection URI.
+            */
+            res.status(400).render('idp_error', { user: null, error: `No active client registration for id: '${client_id}'` });
+            return;    
         }
 
         // Check that client has sent the correct redirect uri:
@@ -142,71 +153,106 @@ function idpRoutes({redisClient, webKeyPub, webKeyPrivate}) {
     router.post("/token", async (req, res) => {
         // See https://developer.okta.com/docs/reference/api/oidc/#token
         let {code, redirect_uri, grant_type, client_id, client_secret, code_verifier} = req.body;
-        if (grant_type != "authorization_code" ) {
-            res.status(401).json({error: 'unsupported_grant_type'});
-            return;
-        }
-        const authReqStored = await redisClient.get('oidc-code:' + code);
-        if (!authReqStored) {
-            res.status(401).json({error: 'invalid_grant'});
-            return;
-        }
-        const authReq = JSON.parse(authReqStored);
-        if (!redirect_uri || authReq.redirect_uri != redirect_uri) {
-            res.status(401).json({error: 'invalid_grant'});
-            return;
-        }
-        // Check Authorization Code flow with PKCE 
-        // See https://datatracker.ietf.org/doc/html/rfc7636#section-4.6 
-        if (authReq.code_challenge) {
-            code_verifier = code_verifier || '';
-            if (generators.codeChallenge(code_verifier) != authReq.code_challenge) {
-                res.status(401).json({ error: 'invalid_grant', error_description: 'S256 code_verifier is not correct' });
-                return;
-            }
-        }
-        
-        // Redis 6.2 supports getdel (https://redis.io/commands/getdel) ..
-        // anyway..
-        await redisClient.del('oidc-code:' + code);
 
-        // Check clients credentials (secret):
+        // Get clients supplied credentials:
         // See https://datatracker.ietf.org/doc/html/rfc6749#section-2.3.1
         // We first check the 'Authorization' header:
         const authHeader = req.get("Authorization")
         if (authHeader) {
+            // If there's an Authorization header it overrides any client-* information
+            // passed in the form-encoded body
             const creds = authBasic.parse(authHeader);
             client_secret = creds ? creds.pass : '';
+            client_id = creds ? creds.name : '';
         }
         // If no such header, we use the secret supplied in the params
         // or an empty string:
         else {
             client_secret = client_secret || '';
         }
-        if (! await bcrypt.compare(client_secret, authReq.secret_hash)) {
-            console.log("Client credentials not valid! Authorization header:"+authHeader);
-            // Error response: https://openid.net/specs/openid-connect-core-1_0.html#TokenErrorResponse
-            res.status(400).json({error: "invalid_request"});
+
+        if (grant_type == "authorization_code") {
+            const authReqStored = await redisClient.get('oidc-code:' + code);
+            if (!authReqStored) {
+                res.status(401).json({error: 'invalid_grant'});
+                return;
+            }
+            const authReq = JSON.parse(authReqStored);
+            if (!redirect_uri || authReq.redirect_uri != redirect_uri) {
+                res.status(401).json({error: 'invalid_grant'});
+                return;
+            }
+            // Check Authorization Code flow with PKCE 
+            // See https://datatracker.ietf.org/doc/html/rfc7636#section-4.6 
+            if (authReq.code_challenge) {
+                code_verifier = code_verifier || '';
+                if (generators.codeChallenge(code_verifier) != authReq.code_challenge) {
+                    res.status(401).json({ error: 'invalid_grant', error_description: 'S256 code_verifier is not correct' });
+                    return;
+                }
+            }
+            
+            // Redis 6.2 supports getdel (https://redis.io/commands/getdel) ..
+            // anyway..
+            await redisClient.del('oidc-code:' + code);
+
+            // Check clients credentials (secret):
+            if (! await bcrypt.compare(client_secret, authReq.secret_hash)) {
+                console.log("Client credentials not valid! Authorization header:"+authHeader);
+                // Error response: https://openid.net/specs/openid-connect-core-1_0.html#TokenErrorResponse
+                res.status(400).json({error: "invalid_request"});
+                return;
+            }
+
+            let idToken = JSON.parse(await redisClient.get("uid:"+authReq.uid));
+            idToken.type = "id_token";
+            idToken.nonce = authReq.nonce;
+            
+            const TTL = 3600;
+            const jwtIdToken = jwt.sign(idToken, webKeyPrivate, {
+                algorithm: 'RS256', expiresIn: TTL,
+                issuer: HOST, audience: authReq.client_id
+            });
+            
+            const jwtAccToken = newAccessToken(idToken.uid, authReq.audience, TTL);
+            let response = {token_type : "Bearer", expires_in : TTL, 
+                            nonce: authReq.nonce,
+                            scope: authReq.scope,
+                            id_token: jwtIdToken, access_token: jwtAccToken};
+            console.log("Token response: %O", response);
+            res.json(response);
             return;
         }
 
-        let idToken = JSON.parse(await redisClient.get("uid:"+authReq.uid));
-        idToken.type = "id_token";
-        idToken.nonce = authReq.nonce;
-        
-        const TTL = 3600;
-        const jwtIdToken = jwt.sign(idToken, webKeyPrivate, {
-            algorithm: 'RS256', expiresIn: TTL,
-            issuer: HOST, audience: authReq.client_id
-        });
-        
-        const jwtAccToken = newAccessToken(idToken.uid, authReq.audience, TTL);
-        let response = {token_type : "Bearer", expires_in : TTL, 
-                        nonce: authReq.nonce,
-                        scope: authReq.scope,
-                        id_token: jwtIdToken, access_token: jwtAccToken};
-        console.log("Token response: %O", response);
-        res.json(response);
+        if (grant_type == "client_credentials") {
+            // Check client_id, and retrieve the client registration info
+            // based on this:
+            let [client_registration, error] = db_client_registration(client_id);
+            if (error) {
+                console.log("%O", e);
+                res.status(500).send("Database error!!");
+                return;
+            }
+
+            // Check clients credentials (secret):
+            if (!client_registration || ! await bcrypt.compare(client_secret, client_registration.pwd_hash)) {
+                console.log("Client credentials not valid! Authorization header:"+authHeader);
+                res.status(400).json({error: "invalid_request"});
+                return;
+            }
+            const TTL = 2 * 3600;
+            
+            const jwtAccToken = newAccessToken(client_id, client_id, TTL);
+            let response = {token_type : "Bearer", expires_in : TTL, 
+                            access_token: jwtAccToken};
+            console.log("CliCreds Token response: %O", response);
+            res.json(response);
+            return;
+        }
+
+        // Only "authorization_code" and "client_credentials" are supported:
+        res.status(401).json({error: 'unsupported_grant_type'});
+        return;
     });
 
 
